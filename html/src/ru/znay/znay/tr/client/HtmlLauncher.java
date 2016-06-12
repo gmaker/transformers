d4434819332417a40d44861fff912920301364ac
gmaker <mr.gmaker@yandex.ru>
2016-06-12 23:17:10 +0300

    structure of code

---
+++ html/src/ru/znay/znay/tr/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package ru.znay.znay.tr.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import ru.znay.znay.tr.TransformersGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TransformersGame();
+        }
+}
